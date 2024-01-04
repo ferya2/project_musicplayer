b5ae8c6551bbc0dfe05e498f3dce09d6731d3678
maulanaakgn <120355453+maulanaakgn@users.noreply.github.com>
2024-01-04 14:43:11 +0700

    update

--- spoticok/spoticok_apk/lib/constants/colors/colors.dart
+++ spoticok/spoticok_apk/lib/constants/colors/colors.dart
@@ -18,8 +18,6 @@ class ColorsCollection {
   static Color abuTextField2 = const Color(0xFF282828);
   static Color abuTextField = const Color(0xFF777777);
   static Color abuBtnText = const Color(0xFF535353);
-<<<<<<< HEAD
-=======
   static Color abuWrap = const Color(0xFF9C9C9C);
   static Color floatingPlayerMusic = const Color(0xFF550A1C);
   static Color textSubList = const Color(0xFFB3B3B3);
@@ -28,7 +26,6 @@ class ColorsCollection {
   static Color darkGradient = const Color(0xFF101010);
   static Color lightGrey = const Color(0xFF3E3F3F);
   static Color reqs4TextGr = const Color(0xFF1ED760); 
->>>>>>> 878b6a056b2d5fe3cd87c2bf394bd3c7cdcc8def
   static Color reqs4CreateBtn = const Color(0xFFF5F5F5);
   static Color lessGreen = const Color(0xFFF5F5F5);
   static Color transparent = Colors.transparent;
